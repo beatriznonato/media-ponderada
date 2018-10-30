3873bd4738bc27d8e85cec19a6063edd4b197559
Beatriz <bia_nonato@icloud.com>
2018-10-30 16:37:30 -0300

    resolucao exercicio

---
+++ mediaPonderada.py
@@ -0,0 +1,8 @@
+peso1 = float(input("Digite o peso da primeira prova: "))
+peso2 = float(input("Digite o peso da primeira prova: "))
+nota1 = float(input("Digite a nota da primeira prova: "))
+nota2 = float(input("Digite a nota da primeira prova: "))
+
+media = ((peso1*nota1) + (peso2*nota2)) / (peso1+peso2)
+
+print("A média do aluno é:",media)
